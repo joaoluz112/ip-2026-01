1ab5a8ed5789fbea2a53b555c09c5e513cc7ca61
joaoluz112 <joao.luz@discente.ufg.br>
2026-03-25 20:28:56 -0300

    Update Ex.go

--- lista2/Ex.go
+++ lista2/Ex.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main(){
+
+	var n int
+
+	fmt.Print("Digite um número: \n")
+	fmt.Scan( &n )
+
+	if ( n %2 == 0){
+		fmt.Print( " O número é par ")
+
+	}else{
+		fmt.Print( " O número é impar ")
+	}
+	
+
+}
