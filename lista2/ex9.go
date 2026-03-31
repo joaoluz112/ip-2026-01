15a0b21f58406c82cdb0480a82b4e38895edff73
joaoluz112 <joao.luz@discente.ufg.br>
2026-03-30 21:51:10 -0300

    Update ex9.go

--- lista2/ex9.go
+++ lista2/ex9.go
@@ -1,1 +1,29 @@
+package main
 
+import "fmt"
+
+func main(){
+
+	var vc, vv float64
+	fmt.Println( " O valor da compra: ")
+	fmt.Scan( &vc )
+
+	switch{
+	case vc < 10:
+		vv = vc + (vc * 0.7)
+		fmt.Printf( " Valor de venda será de R$ %f", vv)
+
+	case vc >= 10 && vc < 30:
+		vv = vc + ( vc * 0.5 )
+		fmt.Printf( " Valor de venda será de R$ %f ", vv)
+
+	case vc >= 30 && vc < 50:
+	    vv = vc + ( vc * 0.4 ) 
+		fmt.Printf( " O valor de venda será de R$ %f ", vv)
+
+	case vc >= 50:
+		vv = vc + ( vc * 0.3 )
+		fmt.Printf( " O valor de venda será de R$ %f ", vv)
+
+	}
+}
