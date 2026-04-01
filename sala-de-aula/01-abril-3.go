2f87478e6778d5ed7e7fdf5a53ccc07070862eac
joaoluz112 <joao.luz@discente.ufg.br>
2026-04-01 20:33:22 -0300

    Create 01-abril-3.go

---
+++ sala-de-aula/01-abril-3.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n1 int
+
+	fmt.Println(" Digite um número: ")
+	fmt.Scan(&n1)
+
+	fmt.Printf(" O fatorial deste número é %d \n", fatorial(n1))
+
+}
+func fatorial(numero int) int {
+	for i := numero - 1; i > 0; i-- {
+		numero *= i
+	}
+
+	return numero
+
+}
