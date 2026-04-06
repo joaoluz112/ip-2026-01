9f2f5d8511d1fde2f02deea33d17850652f6216a
joaoluz112 <joao.luz@discente.ufg.br>
2026-04-06 16:54:46 -0300

    Create ex13.go

---
+++ lista01/ex13.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nota float64
+	var conceito string
+
+	fmt.Scan(&nota)
+
+	if nota >= 9.0 && nota <= 10.0 {
+		conceito = "A"
+	} else if nota >= 7.5 && nota < 9.0 {
+		conceito = "B"
+	} else if nota >= 6.0 && nota < 7.5 {
+		conceito = "C"
+	} else {
+		conceito = "D"
+	}
+
+	fmt.Printf("NOTA = %.1f CONCEITO = %s\n", nota, conceito)
+}
