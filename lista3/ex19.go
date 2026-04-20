0670e9f765e3c6856a758a37a6289a41e0273016
joaoluz112 <joao.luz@discente.ufg.br>
2026-04-20 16:57:13 -0300

    Create ex19.go

---
+++ lista3/ex19.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j > i {
+				fmt.Printf("(%d,%d) ", i, j)
+			}
+		}
+		fmt.Println()
+	}
+}
